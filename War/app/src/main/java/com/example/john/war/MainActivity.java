311ace8ea6a8fac5e73368e56c7f0a0b0ad1b9b9
John <johntp3@outlook.com>
2018-04-27 22:57:20 -0400

    commit after update

--- War/app/src/main/java/com/example/john/war/MainActivity.java
+++ War/app/src/main/java/com/example/john/war/MainActivity.java
@@ -9,15 +9,16 @@ public class MainActivity extends AppCompatActivity {
     //card structure: H 2-A, C 2-A, S 2-A, D 2-A
     int[] regularCards = {2,3,4,5,6,7,8,9,10,11,12,13,14,2,3,4,5,6,7,8,9,10,11,12,13,14,2,3,4,5,6,7,
             8,9,10,11,12,13,14,2,3,4,5,6,7,8,9,10,11,12,13,14};
-    int[] drawableRes = {R.drawable.H2, R.drawable.H3, R.drawable.H4, R.drawable.H5, R.drawable.H6,
-            R.drawable.H7, R.drawable.H8, R.drawable.H9, R.drawable.H10, R.drawable.HJ, R.drawable.HQ,
-            R.drawable.HK, R.drawable.HA, R.drawable.C2, R.drawable.C3, R.drawable.C4, R.drawable.C5,
-            R.drawable.C6, R.drawable.C7, R.drawable.C8, R.drawable.C9, R.drawable.C10, R.drawable.CJ,
-            R.drawable.CQ, R.drawable.CK, R.drawable.CA, R.drawable.S2, R.drawable.S3, R.drawable.S4,
-            R.drawable.S5, R.drawable.S6, R.drawable.S7, R.drawable.S8, R.drawable.S9, R.drawable.S10,
-            R.drawable.SJ, R.drawable.SQ, R.drawable.SK, R.drawable.SA, R.drawable.D2, R.drawable.D3,
-            R.drawable.D4, R.drawable.D5, R.drawable.D6, R.drawable.D7, R.drawable.D8, R.drawable.D9,
-            R.drawable.D10, R.drawable.DJ, R.drawable.DQ, R.drawable.DK, R.drawable.DA};
+    int[] drawableRes = {R.drawable.h2, R.drawable.h3, R.drawable.h4, R.drawable.h5, R.drawable.h6,
+            R.drawable.h7, R.drawable.h8, R.drawable.h9, R.drawable.h10, R.drawable.hj, R.drawable.hq,
+            R.drawable.hk, R.drawable.ha, R.drawable.c2, R.drawable.c3, R.drawable.c4, R.drawable.c5,
+            R.drawable.c6, R.drawable.c7, R.drawable.c8, R.drawable.c9, R.drawable.c10, R.drawable.cj,
+            R.drawable.cq, R.drawable.ck, R.drawable.ca, R.drawable.s2, R.drawable.s3, R.drawable.s4,
+            R.drawable.s5, R.drawable.s6, R.drawable.s7, R.drawable.s8, R.drawable.s9, R.drawable.s10,
+            R.drawable.sj, R.drawable.sq, R.drawable.sk, R.drawable.sa, R.drawable.d2, R.drawable.d3,
+            R.drawable.d4, R.drawable.d5, R.drawable.d6, R.drawable.d7, R.drawable.d8, R.drawable.d9,
+            R.drawable.d10, R.drawable.dj, R.drawable.dq, R.drawable.dk, R.drawable.da};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
