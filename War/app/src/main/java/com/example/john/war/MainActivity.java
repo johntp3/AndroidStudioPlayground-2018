443b1fb07a5c67dd6ef87f085bec9a64664a4b3e
John <johntp3@outlook.com>
2018-05-03 19:01:46 -0400

    introduction to Card Object

--- War/app/src/main/java/com/example/john/war/MainActivity.java
+++ War/app/src/main/java/com/example/john/war/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.john.war;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import java.util.Queue;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     //card structure: H 2-A, C 2-A, S 2-A, D 2-A
@@ -18,10 +23,29 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.sj, R.drawable.sq, R.drawable.sk, R.drawable.sa, R.drawable.d2, R.drawable.d3,
             R.drawable.d4, R.drawable.d5, R.drawable.d6, R.drawable.d7, R.drawable.d8, R.drawable.d9,
             R.drawable.d10, R.drawable.dj, R.drawable.dq, R.drawable.dk, R.drawable.da};
+    ImageView leftCard;
+    ImageView rightCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // creates random integer 0-51
+        Random rand = new Random();
+        int num = rand.nextInt(52);
+        //connecting back end code to front end
+        leftCard = findViewById(R.id.playerOne);
+
+        int imageResource = drawableRes[num];
+        Drawable res = ResourcesCompat.getDrawable(getResources(), imageResource, null);
+        leftCard.setImageDrawable(res);
+
+        num = rand.nextInt(52);
+        rightCard =findViewById(R.id.playerTwo);
+        imageResource = drawableRes[num];
+        res = ResourcesCompat.getDrawable(getResources(), imageResource, null);
+        rightCard.setImageDrawable(res);
+
+        // make a Card(int value, int img) class, generate all 52 cards and add them to the player queues.
     }
 }
